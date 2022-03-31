3433f0cae064fb0ae9d164684d6a853afec4e1c9
akkago <riabov.s@outlook.com>
2022-03-31 22:26:53 +0300

    solve task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Web3 from 'web3';
 
 function App() {
+  let [publicAdress, setPublicAddress] = useState('');
+  let w3;
+
+  async function connect() {
+    try {
+      const accounts = await w3.eth.getAccounts();
+
+      if (!accounts[0]) {
+        alert('Кошелёк не найден, войдите в Метамаск создайте кошелёк и подключите этот сайт');
+      }
+      else {
+        setPublicAddress(accounts[0]);
+      }
+    } catch (err) { console.error(err); }
+  }
+
+  const onClick = () => {
+    w3 = checkAndInstantiateWeb3()
+    connect();
+  }
+
+  function checkAndInstantiateWeb3() {
+    try {
+      if (window.web3 !== 'undefined') {
+        console.log("Using Metamask's web3 provider");
+        return new Web3(window.web3.currentProvider);
+      } else {
+        console.warn('No web3 detected. Falling back to http://localhost:8545.');
+        return new Web3(new this.Web3.providers.HttpProvider('http://localhost:8545'));
+      }
+    } catch (err) { console.error(err); }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={onClick}>
+        Get public key
+      </button>
+      <div>public key: {publicAdress}</div>
     </div>
   );
 }
